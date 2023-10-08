048c71ba610ab7bc1cc8008598a440a9f1d1aa51
Osama Salman <osama.salman99@gmail.com>
2023-10-08 15:58:42 +0300

    fix(logging): Mask PCI Security-Sensitive Information
    Mask PCI security-sensitive information

--- core/src/constant/LogMaskingRegex.ts
+++ core/src/constant/LogMaskingRegex.ts
@@ -34,6 +34,6 @@ export const LogMaskingRegex = {
   AUTH_USERNAME_REGEX: `(?<=${Authentication.USERNAME}:\\s('|"))\\S+(?=('|"))`,
   AUTH_PASSWORD_REGEX: `(?<=${Authentication.PASSWORD}:\\s('|"))\\S+(?=('|"))`,
   ACCESS_TOKEN_REGEX: '(?<=access_token: (\'|"))\\S+(?=(\'|"))',
-  NUMBER_FIELD_REGEX: `(?<=(number): ('|"))\\S{15,16}(?=('|"))`,
-  PCI_FIELDS_REGEX: `(?<=(${PCIRelatedFields.join('|')}): ('|"))\\S+(?=('|"))`
+  NUMBER_FIELD_REGEX: `(?<=['"]number['"]:\\s*['"])[^\\s'"]{15,16}(?=(['"]))`,
+  PCI_FIELDS_REGEX: `(?<=['"](${PCIRelatedFields.join('|')})['"]:\\s*['"])[^\\s'"]+(?=(['"]))`
 } as const
--- core/test/logging/LogMasker.test.ts
+++ core/test/logging/LogMasker.test.ts
@@ -38,31 +38,31 @@ describe('LogMasker', function () {
   })
 
   it('given (15-16)-digit numbers apply number field mask', async function () {
-    const text: string = `number: '012345678912345', number: '0123456789123456'`
+    const text: string = `'number': '012345678912345', "number": '0123456789123456'`
 
-    expect(mask(text)).toEqual(`number: '${LoggingMessage.OMITTED}', number: '${LoggingMessage.OMITTED}'`)
+    expect(mask(text)).toEqual(`'number': '${LoggingMessage.OMITTED}', "number": '${LoggingMessage.OMITTED}'`)
   })
 
   it('given non-(15-16)-digit numbers apply no masks', async function () {
-    const text: string = `number: '01234567891234', number: '01234567891234567', number: '12345'`
+    const text: string = `'number': '01234567891234',"number": '01234567891234567', 'number': '12345'`
 
     expect(mask(text)).toEqual(text)
   })
 
-  it('given (15-16)-digit numbers apply number field mask', async function () {
-    const text: string = `pin: 'pin_value',` +
-      `card_number: 'card_number_value',` +
-      `security_code: 'security_code_value',` +
-      `account_number: 'account_number_value',` +
-      `card_avs_response: 'card_avs_response_value',` +
-      `card_cvv_response: 'card_cvv_response_value',`
+  it('given PCI-related fields apply PCI fields mask', async function () {
+    const text: string = `"pin": 'pin_value',` +
+      `'card_number': 'card_number_value', ` +
+      `"security_code": "security_code_value",` +
+      `'account_number': "account_number_value", ` +
+      `"card_avs_response": 'card_avs_response_value',` +
+      `'card_cvv_response': 'card_cvv_response_value'`
 
-    const expectedText: string = `pin: '${LoggingMessage.OMITTED}',` +
-      `card_number: '${LoggingMessage.OMITTED}',` +
-      `security_code: '${LoggingMessage.OMITTED}',` +
-      `account_number: '${LoggingMessage.OMITTED}',` +
-      `card_avs_response: '${LoggingMessage.OMITTED}',` +
-      `card_cvv_response: '${LoggingMessage.OMITTED}',`
+    const expectedText: string = `"pin": '${LoggingMessage.OMITTED}',` +
+      `'card_number': '${LoggingMessage.OMITTED}', ` +
+      `"security_code": "${LoggingMessage.OMITTED}",` +
+      `'account_number': "${LoggingMessage.OMITTED}", ` +
+      `"card_avs_response": '${LoggingMessage.OMITTED}',` +
+      `'card_cvv_response': '${LoggingMessage.OMITTED}'`
 
     expect(mask(text)).toEqual(expectedText)
   })
