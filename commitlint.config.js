cf4f2d0abd93559c778fff9d7a30bc7fe2fe4a80
Omar Al-Jarrah <oaljarrah@expediagroup.com>
2023-12-13 16:57:39 +0300

    chore(github): update rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'header-max-length': [2, 'always', 250],
-        'type-empty': [2, 'never'],
-        'type-case': [2, 'always', 'lower-case'],
-        'type-enum': [2, 'always', ['feat', 'refactor', 'fix', 'docs', 'test', 'chore', 'ci', 'release']],
-        'scope-empty': [2, 'never'],
-        'subject-empty': [2, 'never'],
-        'body-leading-blank': [2, 'always'],
-        'body-max-line-length': [2, 'always', 100]
-    }
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 250],
+    'type-empty': [2, 'never'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-enum': [2, 'always', ['feat', 'refactor', 'fix', 'docs', 'test', 'chore', 'ci', 'release']],
+    'subject-empty': [2, 'never'],
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [2, 'always', 100]
+  }
 };
